c8d7292075275674a646a76a60d2858344fdc577
Simmer999 <fearlessleader5900@gmail.com>
2022-08-12 10:49:07 -0600

    axios.get to 127.0.0.1

--- server/services/render.js
+++ server/services/render.js
@@ -14,7 +14,7 @@ exports.homeRoutes = (req, res) => {
 
 exports.indexRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:5554/api/users')
+    axios.get('http://127.0.0.1:5554/api/users')
         .then(function(response){
             res.render('memoryApp/bookList', { users : response.data });
         })
