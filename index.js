22a3145ed3ae9f345e824ee5c4cf86a990e3b004
Simmer999 <fearlessleader5900@gmail.com>
2022-08-12 18:02:42 -0600

    add dns.setServer

--- index.js
+++ index.js
@@ -26,7 +26,17 @@ app.use(passport.session())
 require('./api/config/passport')(passport);
 
 //-----------------------------------------------
-
+const { Resolver } = require('dns');
+const resolver = new Resolver();
+  
+// Set Servers
+resolver.setServers(['11.11.1.11']);
+// dns.setServers([
+//     '4.4.4.4',
+//     '[2001:4860:4860::8888]',
+//     '4.4.4.4:1053',
+//     '[2001:4860:4860::8888]:1053',
+//   ]);
 // mongodb connection
 const connectDB = require('./server/database/connection');
 
