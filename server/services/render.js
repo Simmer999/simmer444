f005af1c33e4832d57587b31ab527583775539c1
Sim <fearlessleader5900@gmail.com>
2022-08-19 22:00:37 -0600

    z

--- server/services/render.js
+++ server/services/render.js
@@ -14,7 +14,7 @@ exports.homeRoutes = (req, res) => {
 
 exports.indexRoutes = (req, res) => {
     // Make a get request to /api/users
-    axios.get('http://localhost:5554/api/users')
+    axios.get('mongodb://localhost:27017/api/users')
         .then(function(response){
             res.render('memoryApp/bookList', { users : response.data });
             // console.log(response.data)
