62acae386a1bae0589923d5aeabba8c8dbce5acb
Sim <fearlessleader5900@gmail.com>
2022-10-15 17:26:13 -0600

    update

--- server/database/connection.js
+++ server/database/connection.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try{
         // mongodb connection string
-        const con = await mongoose.connect(process.env.DB || 'http://localhost:3000/api/users', {
+        const con = await mongoose.connect('mongodb+srv://simmmer:snoops22@cluster0.34ahm.mongodb.net/experiment?retryWrites=true&w=majority'
+            //  || 'http://localhost:3000/api/users'
+             , {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useFindAndModify: false,
